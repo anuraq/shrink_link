1fac8a3c865aea995e5406cfc5d2edce2a70d198
unknown <yoyoanusingh27@gmail.com>
2020-07-16 23:50:18 +0530

    Repo exists

--- lib/shrink_link/my_plug.ex
+++ lib/shrink_link/my_plug.ex
@@ -2,6 +2,7 @@ defmodule ShrinkLink.MyPlug do
   use Plug.Router
   alias ShrinkLink.Repo
   alias ShrinkLink.Links
+  import Ecto.Query, only: [from: 2]
 
   plug :match
   plug Plug.Parsers, parsers: [:json],
@@ -18,13 +19,18 @@ defmodule ShrinkLink.MyPlug do
     blob = String.downcase(blob)
     {:ok, map} = JSON.Encoder.encode(%{blob: blob, url: url})
     #changeset = Links.changeset(%Links{}, %{blob: blob, url: url})
+    c = Repo.exists?(from u in ShrinkLink.Links, where: u.blob == ^blob)
     #Repo.insert(changeset)
-    IO.puts("Blob is #{blob} and Url is #{url}")
-    send_resp(conn, 200, map)
+    IO.puts("Blob is #{blob} and Url is #{url} and count is #{c}")
+    update_resp_header(conn,"content-type","application/json; charset=utf-8",&(&1 <> "; charset=utf-8")) |> send_resp( 200, map)
   end
 
   match _ do
     send_resp(conn, 404, "oops")
+  end
+
+  defp generate_nanoid do
+
   end
 
 
